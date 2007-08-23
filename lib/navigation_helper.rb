743b42a1abc0f510f4000f08a1a2f81ddad5adb3
eruera <eruera@68f1b24a-cf1b-0410-8d2d-c7fc56e366a6>
2007-08-23 00:43:30 +0000

    update savatrise plugin for better route recognition
    git-svn-id: svn://dev.webfu.co.nz/base/vendor/plugins/savatrise@3244 68f1b24a-cf1b-0410-8d2d-c7fc56e366a6

--- lib/navigation_helper.rb
+++ lib/navigation_helper.rb
@@ -14,20 +14,23 @@ module NavigationHelper
 
   def active_tab(tab, active = @active_tab)
     return if tab.nil?
-    ' class="selected"' if tab.to_sym == active
+    ' class="selected"' if active.include?(tab.to_s)
   end
 
   def tab(name, options = {}, html_options = nil, *parameters_for_method_reference, &proc)
     if options.is_a?(Hash) && (options[:controller].nil? || options[:action].nil?)
       raise ArgumentError, 'You must include a controller and action!'
     end
+
+    rs = ActionController::Routing::Routes
+
     # give the li's a class if theres a child
-    klass = active_tab "#{options[:controller]}_#{options[:action]}"
+    klass = active_tab rs.generate(options)
 
     li_contents = link_to(name, options, html_options, *parameters_for_method_reference)
 
     if block_given?
-      parent_klass = active_tab @parent_tab, @parent_tab
+      parent_klass = active_tab(@parent_tab, @parent_tab)
       klass = parent_klass unless parent_klass.nil?
 
       concat("<li#{klass}>#{li_contents}<ul>", proc.binding)
--- lib/savalicious.rb
+++ lib/savalicious.rb
@@ -3,16 +3,15 @@ module Savalicious
   private
 
     def define_tab
-      set_active_tab "#{controller_name}_#{action_name}"
+      set_active_tab request.path
     end
 
     def set_active_tab tab_name
-      @active_tab = tab_name.to_sym
+      @active_tab = (tab_name.is_a?(Array))? tab_name : tab_name.to_s
     end
-    
+
     def set_parent_tab tab_name
-      @parent_tab = tab_name.to_sym
+      @parent_tab = (tab_name.is_a?(Array))? tab_name : tab_name.to_s
     end
-    
 
 end
