29e0de4aaa5cf44999855fedada72c0d49c6b9a0
eruera <eruera@68f1b24a-cf1b-0410-8d2d-c7fc56e366a6>
2008-01-20 20:42:10 +0000

    update savatrise with some more config options
    git-svn-id: svn://dev.webfu.co.nz/base/vendor/plugins/savatrise@4159 68f1b24a-cf1b-0410-8d2d-c7fc56e366a6

--- init.rb
+++ init.rb
@@ -1,3 +1,18 @@
 # Include hook code here
+ENV['TZ'] = 'Pacific/Auckland'
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  error_style = "errorField"
+  if html_tag =~ /<(input|textarea|select)[^>]+class=/
+    style_attribute = html_tag =~ /class=['"]/
+    html_tag.insert(style_attribute + 7, "#{error_style} ")
+  elsif html_tag =~ /<(input|textarea|select)/
+    first_whitespace = html_tag =~ /\s/
+    html_tag[first_whitespace] = " class=\"#{error_style}\" "
+  end
+  html_tag
+end
+
+
 ActionController::Base.send :include, Savalicious
 ActionView::Base.send :include, Savatrise
